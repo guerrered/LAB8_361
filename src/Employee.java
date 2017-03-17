97b865f8c42b4a4b742c81aeb6ca6c3277a860b1
dishenwen <dwen@uwm.edu>
2017-03-17 15:33:02 -0500

    Done

--- src/Employee.java
+++ src/Employee.java
@@ -18,7 +18,7 @@ public class Employee {
 	
 	public String toString(){
 		String str="";
-		str = str+lastName+", "+firstName+ ", "+ Phone + ", " + Department + ", " + Gender + ", " + Title;
+		str = str+"LastName: "+lastName+", First Name: "+firstName+ ", CellPhone: "+ Phone + ", Department: " + Department + ", Gender: " + Gender + ", Title: " + Title;
 		return str;
 	}
 	
